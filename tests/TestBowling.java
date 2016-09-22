7e910f0dd854624fc66741065a4987e540d9135f
somename <someemail>
2016-09-22 15:44:32 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -6,7 +6,8 @@ public class TestBowling {
 
 	@Test
 	public void ScoreTest() {
-		BowlingGame bowl = new BowlingGame;
+		BowlingGame bowl = new BowlingGame();
+		bowl.addFrame(1,0);
 		//fail("Not yet implemented");
 	}
 
