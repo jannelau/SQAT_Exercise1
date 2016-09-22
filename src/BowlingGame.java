d7bb5dd9cb53a68cf3c01971a8e45d49081e4b08
somename <someemail>
2016-09-22 15:00:34 +0300

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -18,12 +18,12 @@ public class BowlingGame {
 	}
 	
 	// Sets the bonus throws at the end of the game
-	public void setBonus(int firstThrow, int secondThrow) {
+	public void setBonus(int firstThrow, int secondThrow) throws BowlingException {
 		//to be implemented
 	}
 	
 	// Returns the game score
-	public int score(){
+	public int score() throws BowlingException{
 		//to be implemented: should return game score 
 		return score;
 	}
